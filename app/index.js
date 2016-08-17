9d19ddb5a7af29a1196a63ec9942b9823fad5b51
CDThomas <cdthomas.92@gmail.com>
2016-08-16 21:59:38 -0400

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,8 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+// Mount the app to the dom using the specified tag
+ReactDOM.render(
+  <div>Hello World</div>,
+  document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+// In webpack.config.js
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+// config for Html webpack plugin
+// Use app/index.html as a template and inject bundled code into the body of the file for dist
+// This will end up with a dist/index.html with a script tag reference index_bundle.js
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    // entry point in /app
+    './app/index.js'
+  ],
+  output: {
+    // output to /dist
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  module: {
+    // loaders to run on files
+    loaders: [
+      // babel loader used for jsx transpilation
+      {test: /\.js$/, include: __dirname + '/app', loader: 'babel-loader'}
+    ]
+  },
+  // plugins go outside of module
+  // Use the Html webpack plugin to generate dist html file from a template
+  plugins: [HtmlWebpackPluginConfig]
+}
